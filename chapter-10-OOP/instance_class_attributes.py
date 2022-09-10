918328b3e067e0bda1f32ae6f30c61d28335e77b
paras koli <paraskoli43@gmail.com>
2022-09-10 21:33:19 +0530

    instance

--- chapter-10-OOP/instance_class_attributes.py
+++ chapter-10-OOP/instance_class_attributes.py
@@ -1,4 +1,11 @@
-a=98
-b=98
-c=a+b
-print(c)
+class Employee:
+    company="Google"
+    salary=100
+
+harry=Employee()
+rajini=Employee()
+#creating instance attribute salary for both the objects 
+#harry.salary=300
+#rajini.salary=400
+print(harry.salary)
+print(rajini.salary)
