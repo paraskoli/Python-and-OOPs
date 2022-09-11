a3dee2c4238a9b8836d5d060b0667c90d1072060
paras koli <paraskoli43@gmail.com>
2022-09-11 20:52:39 +0530

    self.y

--- chapter-10-OOP/instance_class_attributes.py
+++ chapter-10-OOP/instance_class_attributes.py
@@ -1,6 +1,7 @@
 class Employee:
     company="Google"
     salary=100
+    
 
 harry=Employee()
 rajini=Employee()
@@ -9,3 +10,5 @@ rajini=Employee()
 #rajini.salary=400
 print(harry.salary)
 print(rajini.salary)
+#below line throws an error as address is not present in the instance/class
+#print(rajini.address)
---
+++ chapter-10-OOP/self.py
@@ -0,0 +1,7 @@
+class Employee:
+    company="Google"
+    def getSalry(self):
+        print("salary is 100k")
+
+harry=Employee()
+harry.getSalry()
