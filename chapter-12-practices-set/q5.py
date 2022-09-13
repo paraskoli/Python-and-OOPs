65a09c9d331bb2252f8f503fa80f13c6c27ad295
paras koli <paraskoli43@gmail.com>
2022-09-13 14:59:39 +0530

    q5.py

---
+++ chapter-12-practices-set/q5.py
@@ -0,0 +1,23 @@
+class Train:
+    def __init__(self,name,fare,seats):
+        self.name=name
+        self.fare=fare
+        self.seats=seats
+
+    def getStatus(self):
+        print(f"The name of the Train is {self.name}")
+        print(f"The sets availabe in the train are {self.seats}")
+
+    def fareInfo(self):
+        print(f"The price of the ticket is : Rs {self.fare}")
+    
+    def bookTickets(self):
+        if (self.seats>0):
+            print(f"Your ticket has been booked! your seat number is {self.seats}")
+            self.seats=self.seats-1
+        else:
+            print("sorry this train is full")    
+intercity=Train("Intercity express: 1405",90,300)
+intercity.getStatus()
+intercity.bookTickets()
+intercity.getStatus()
