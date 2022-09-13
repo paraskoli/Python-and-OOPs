9a1adecf21160931f3965c081e3ca2289f97ffd1
paras koli <paraskoli43@gmail.com>
2022-09-13 14:24:50 +0530

    q2.py

---
+++ chapter-12-practices-set/q2.py
@@ -0,0 +1,17 @@
+class claculator:
+    def __init__(self,num):
+        self.number=num
+    
+    def square(self):
+        print(f"The square of {self.number} is {self.number **2}")
+
+    def squareRoot(self):
+        print(f"The squareRoot of {self.number} is {self.number **0.5}")
+
+    def cube(self):
+          print(f"The cube of {self.number} is {self.number **3}")
+
+a=claculator(9)
+a.square()
+a.squareRoot()
+a.cube()
