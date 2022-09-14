6882e37e398440664a167b59bb69e853efc01e1e
paras koli <paraskoli43@gmail.com>
2022-09-14 22:06:18 +0530

    propert_decoratory.py

--- chapter-11/property_decorator.py
+++ chapter-11/property_decorator.py
@@ -1,2 +1,20 @@
 class Employee:
-     
+    company="Microsoft"
+    salary=5500
+    salarybonus=504
+    # totalsalary=6000
+
+    @property
+    def totalSalary(self):
+        return self.salary+self.salarybonus
+
+    @totalSalary.setter
+    def totalSalary(self,val):
+        self.salarybonus=val - self.salary
+
+e=Employee()
+#print(e.totalsalary()) #if property is not included
+print(e.totalSalary)
+e.totalSalary=5800
+print(e.salary)
+print(e.salarybonus)
