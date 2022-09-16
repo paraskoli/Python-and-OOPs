e0d89f30c4af60f1a1fce2cf1009e58ec77c8db1
paras koli <paraskoli43@gmail.com>
2022-09-16 13:38:46 +0530

    operator_overloading.py

--- chapter-11/operator_overloading.py
+++ chapter-11/operator_overloading.py
@@ -4,9 +4,15 @@ class Number:
 
     def __add__(self,num2):
         print("Lets add")
-        return 300
+        return self.num+num2.num
+
+    def __mul__(self,num2):
+        print("Lets Multiply")
+        return self.num*num2.num
 
 n1=Number(4)
 n2=Number(6)
 sum=n1+n2
+mul=n1*n2
 print(sum)
+print(mul)
