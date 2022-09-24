ff3bcf9f9cae5f49cfc10c69b98600c2ed26ff97
paras koli <paraskoli43@gmail.com>
2022-09-24 09:57:31 +0530

    q4.py

--- chapter11 practice set/q4.py
+++ chapter11 practice set/q4.py
@@ -1,7 +1,4 @@
 #(a+bi)(c+di)=(ac-bd)+(ad+bc)
-import bdb
-
-
 class Complex:
     def __init__(self,r,i):
         self.real=r
@@ -11,8 +8,9 @@ class Complex:
         return Complex(self.real+c.real,self.imaginary+c.imaginary)
 
     def __mul__(self,c):
-        mlReal=(ac-bd)
-        return Complex(self.real*c.real,self.imaginary+c.imaginary)
+        mulReal=self.real*c.real-self.imaginary*c.imaginary
+        mulImg=self.real*c.imaginary+self.imaginary*c.real
+        return Complex(mulReal,mulImg)
 
     def __str__(self):
         return f"{self.real}+{self.imaginary}i"
@@ -20,3 +18,4 @@ class Complex:
 c1=complex(1,5)
 c2=complex(8,5)
 print(c1+c2)
+print(c1*c2)
